aa79dbcc2f4b37f0b70d3417eed6270bc1d24927
Gaetan <gaetan@ictpourtous.com>
2025-11-28 14:33:51 +0100

    Rename files

--- templates/Postfix/scripts/postfix_get_spool.sh
+++ templates/Postfix/scripts/postfix_get_spool.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-###############################################################
-# postfix_get_spool.sh - Get number of messages currently in postfix queue dirs
-#                        in JSON format
-#
-# Version: 0.2
-# Author: Robin Roevens
-# Changelog:
-#   2023-12-14: Create
-#   2024-01-10: Get postfix queue basedir from config
-#
-
 postfix_config=/etc/postfix/main.cf
 basedir=$(grep -e ^queue_directory $postfix_config | sed 's/.\+=[[:space:]]\?\(.\+\)/\1/')
 [[ $basedir ]] || basedir=/var/spool/postfix  # Fallback to default when not found in config
